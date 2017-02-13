61532599dce1992eb42bf8921c0297be8ace9ee8
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-02-13 15:01:51 -0600

    Update router.js

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
+
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
   	res.send({ hi: 'there' });
@@ -11,8 +12,4 @@ module.exports = function(app) {
 
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
-
-	// app.get('/', function(req, res, next) {
-	// 	res.send(['waterbottle', 'phone', 'paper']);
-	// });
 }
